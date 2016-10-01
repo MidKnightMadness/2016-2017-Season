fedc5c81f76cd6cba103de95e90861c5da4e059b
Joshua M. Atler <joshatler@gmail.com>
2016-10-01 13:31:16 -0700

    Created a simple joystick control program for the little carnival bot.

---
+++ TeamCode/src/main/java/org/tka/robotics/LittleCarnivalBot.java
@@ -0,0 +1,38 @@
+package org.tka.robotics;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Joshua on 9/28/2016.
+ */
+
+@TeleOp(name = "LittleCarnivalBot")
+public class LittleCarnivalBot extends OpMode {
+    private DcMotor left, right;
+
+    @Override
+    public void init() {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+
+        left.resetDeviceConfigurationForOpMode();
+        right.resetDeviceConfigurationForOpMode();
+
+        left.setDirection(DcMotorSimple.Direction.FORWARD);
+        right.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        left.setPower(gamepad1.left_stick_y);
+        right.setPower(gamepad1.right_stick_y);
+
+        telemetry.addData("left", gamepad1.left_stick_y);
+        telemetry.addData("right", gamepad1.right_stick_y);
+        telemetry.update();
+    }
+}
